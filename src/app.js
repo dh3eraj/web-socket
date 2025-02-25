1261af6d099aec413e5611df03edde682f77d502
dh3eraj <dheerajgupta200002@gmail.com>
2025-02-25 15:15:44 +0530

    done

--- src/app.js
+++ src/app.js
@@ -1,11 +1,19 @@
 const express = require('express');
+const {WebSocketServer} = require('ws');
 
 const app = express();
 
 
-
-
-app.listen(8080,
+const server = app.listen(8080,
     () => { 
         console.log(`server is listening on port 8080...`);
     });
+    const wss = new WebSocketServer({server});
+wss.on('connection', (ws) => {
+    ws.on('error', console.error);
+
+    ws.on('message', (data) => {
+        console.log("data : " + data);
+        ws.send(`{ from: 'Server', text: 'Welcome!', createdAt: ${Date()} }`);
+    });
+});
